96538ecfb58a5343e82627a6bc10043758b22d1d
sidneyvmml <sidneyvmml@gmail.com>
2021-05-03 23:14:25 -0300

    Projeto Criado

---
+++ src/main/java/com/sidney/springboot/EstudosSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.sidney.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosSpringboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosSpringboot1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidney/springboot/EstudosSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidney.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosSpringboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
